bd8c60719297f99f8ca726862a8e6d5fc0dd4a07
andbark <89865399+andbark@users.noreply.github.com>
2025-03-13 11:16:27 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,7 @@
-import { createClient as createClientBrowser } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js"
 
-export function createClient() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-  if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error('Missing Supabase environment variables')
-  }
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-  return createClientBrowser(supabaseUrl, supabaseAnonKey)
-}
